0121a7af9ffcc8e0244f90427987a39f406a9bd9
Campbell Shirlaw <campbellshirlaw@Campbells-MacBook-Air.local>
2025-07-15 14:15:08 +0100

    Updated homepage title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <main>
       <HeroBanner
         title="Energy for People, Progress for Society"
-        subtitle="Exploring and producing oil and gas in the Danish North Sea"
+        subtitle="Exploring and producing oil and gas "
       />
       <section className="intro container">
         <p className="text-lg leading-relaxed mb-6">
